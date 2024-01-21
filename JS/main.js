19120e524efa6af23ac13e6de3e2e3b92673e486
Elisaul Velasquez <elisaulvz@gmail.com>
2024-01-20 21:24:04 -0300

    preentrega 2

--- JS/main.js
+++ JS/main.js
@@ -1,40 +1,58 @@
+const canasto = []
+
+const libros = [{codigo: 10, titulo: 'El libro rojo', precio:2100},
+                {codigo: 20, titulo: 'La revista verde', precio:552},
+                {codigo: 30, titulo: 'El cuaderno azul', precio:6530},
+                {codigo: 40, titulo: 'La carpeta amarilla', precio:4525}, ]
+
 saludoInicio()
 
-function saludoInicio() {
-    let saludo = confirm("bienvenido, deseas consultar algun libro?") 
-    if(saludo){
-        libroDisponible()
-    }  
+function saludoInicio(){
+    let saludo = confirm("Bienvenido, deseas consultar algun libro?")
+    if(!saludo){
+        alert("Has cancelado, Vuelve pronto.")
+    }
     else{
-        alert("Adios, vuelve pronto")
-    } 
-}
-function libroDisponible() {
-    let consultaLibro;
-
-    while (true) {
-        consultaLibro = prompt("Ingresa el código del libro");
-
-        if (consultaLibro === null) {
-            alert("Has cancelado. Adios");
-            break; 
-        } else if (consultaLibro === "") {
-            alert("No ingresaste ningún dato. Por favor, ingresa un código.");
-        } else {
-            consultaLibro = parseInt(consultaLibro);
-
-            if (isNaN(consultaLibro)) {
-                alert("Ingresaste un dato no válido. Por favor, ingresa un código válido.");
-            } else {
-                if (consultaLibro !== 10 && consultaLibro !== 20 && consultaLibro !== 30 && consultaLibro !== 40) {
-                    alert("El libro no está disponible");
-                } else {
-                    alert("El libro está disponible");
-                }
-                break;
-            }
-        }
+        ingresaCodigo()
     }
 }
 
+function ingresaCodigo(){
+    let codigoProducto = prompt("Ingresa el coódigo del libro")
+    let libroEncontrado = libros.find(libro=> libro.codigo === parseInt(codigoProducto))
+    if(libroEncontrado){
+        alert(`Libro encontrado: ${libroEncontrado.titulo}`)
+        canasto.push(libroEncontrado)
+        mostrarCanasto()
+    }
+    else if(codigoProducto === null){
+        alert("Has cancelado, vuelve pronto")
+        return
+    }
+    else{
+        alert("Libro no disponible, ingresa otro código.")
+        ingresaCodigo()
+        return
+    }
 
+    let continuaCompra = confirm("Deseas agregar otro libro?")
+    if(continuaCompra){
+        ingresaCodigo()
+    }
+    else{
+        finalizaCompra()
+    }
+}
+
+function mostrarCanasto() {
+    console.clear()
+    console.table(canasto)
+}
+function finalizaCompra() {
+    let totalCompra = canasto.reduce((total, libro) => total + libro.precio, 0);
+
+    console.clear()
+    console.table(canasto)
+    console.log("🛍️ El costo total de tu compra es: $ " + totalCompra);
+    console.log("Muchas gracias por tu compra.")
+}
